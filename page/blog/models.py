8b1ce88c40d318fa8d0b09dfddf220a46b12e0af
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-09 23:03:36 +0200

    adding new ORM

--- page/blog/models.py
+++ page/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+#from django.contrib.auth.models import User
 
-# Create your models here.
+#ORM object relationar mapper
+class  Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+    #author = models.ForeignKey(User, on_delete=True)
