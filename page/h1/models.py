6d4e332a1c68ff5317003330f73f7399226ee310
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-12 21:15:59 +0200

    adding new ORM to h1

--- page/h1/models.py
+++ page/h1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length = 200, null = True)
+    phone = models.CharField(max_length = 200, null = True)
+    email = models.CharField(max_length = 200, null = True)
+    date = models.DateTimeField(auto_now_add=True, null = True)
+
+
+
+
