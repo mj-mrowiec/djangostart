2be78b3b65a8aa3dd707a9de91705f1efd3e1649
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-12 21:47:17 +0200

    adding two tables into ORM

--- page/h1/models.py
+++ page/h1/models.py
@@ -2,12 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length = 200, null = True)
     phone = models.CharField(max_length = 200, null = True)
     email = models.CharField(max_length = 200, null = True)
     date = models.DateTimeField(auto_now_add=True, null = True)
 
+    def __str__(self):
+        return self.name
 
 
+class Product(models.Model):
+    CATEGORY = (
+        ('Indor','Indor'),
+        ('Out Dor','Out Dor')
+    )
+    name = models.CharField(max_length = 200, null = True)
+    price = models.FloatField(null = True)
+    category = models.CharField(max_length = 200, null = True, choices = CATEGORY)
+    description = models.CharField(max_length = 200, null = True)
+    date_created = models.DateTimeField(auto_now_add = True, null = True)
 
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered','Delivered'),
+        ('Returned','Returned')
+    )
+    #customer = a
+    #product = b
+    date_created = models.DateTimeField(auto_now_add = True, null = True)
+    status = models.CharField(max_length = 200, null = True, choices = STATUS)
