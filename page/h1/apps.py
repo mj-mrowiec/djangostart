59f9e1b969d66d905a0f03b3b6cf7437fa67d698
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-12 20:17:53 +0200

    adding new apps h1 and h2

---
+++ page/h1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class H1Config(AppConfig):
+    name = 'h1'
---
+++ page/h2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class H2Config(AppConfig):
+    name = 'h2'
