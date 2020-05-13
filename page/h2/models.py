babae8ec10f2bb283146161004523a23fa6d864d
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-13 21:05:39 +0200

    adding customer ORM under h2

--- page/h2/models.py
+++ page/h2/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    CUST_TYPE = (('PRIVATE','PRIVATE'),
+                ('BUSINESS','BUSINESS'))
+    name = models.CharField(max_length = 200, null = True)
+    phone = models.CharField(max_length = 200, null = True)
+    email = models.CharField(max_length = 200, null = True)
+    date_creation = models.DateTimeField(auto_now=True, null = True)
+    customer_type = models.CharField(max_length = 200, choices = CUST_TYPE)
+
+    def __str__(self):
+        return self.name
