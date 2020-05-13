5eca2e54aee393dde74499c95b853e8dd87b8c7d
mj-mrowiecm <mjmrowiec@gmail.com>
2020-05-13 21:13:51 +0200

    Product ORM under h2

--- page/h2/models.py
+++ page/h2/models.py
@@ -9,8 +9,21 @@ class Customer(models.Model):
     name = models.CharField(max_length = 200, null = True)
     phone = models.CharField(max_length = 200, null = True)
     email = models.CharField(max_length = 200, null = True)
-    date_creation = models.DateTimeField(auto_now=True, null = True)
+    date_creation = models.DateTimeField(auto_now_add=True, null = True)
     customer_type = models.CharField(max_length = 200, choices = CUST_TYPE)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    PROD_CAT = (('Domestic','Domestic'),
+                ('Out Dor','Out Dor'),
+                ('Distinct','Distinct')
+    )
+    name = models.CharField(max_length = 200, null = True)
+    category = models.CharField(max_length = 200, choices = PROD_CAT)
+    price = models.FloatField(null = False)
+    date_add = models.DateTimeField(auto_now_add=True, null = True)
 
     def __str__(self):
         return self.name
