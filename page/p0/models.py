96b857085ac13f2dfcfe6a4d514d9800c6339d40
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-18 21:49:32 +0200

    ORM profiles post_save

--- page/p0/models.py
+++ page/p0/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+
+class Profiles(models.Model):
+    name = models.CharField(max_length = 200, null = True)
+
+    def __str__(self):
+        return self.name
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.ocjecs.created(User = instance)
+        print('Profile has been created')
+
+def updated_profile(sender, instance, created, **kwargs):
+    if created == False:
+        instance.Profile.save()
+
+post_save.connect(create_profile, sender=User)
+post_save.connect(create_profile, sender=User)
