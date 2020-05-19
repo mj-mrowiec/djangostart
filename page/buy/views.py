6fa56ca304b3d023289b7b8ba42031c648981781
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-19 20:52:36 +0200

    remove profiles p0 and h1

--- page/buy/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'buy/buyhome.html', {'title':'My new title'})
--- page/p0/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class P0Config(AppConfig):
-    name = 'p0'
