68343174b66a13db931772dccbf312d24cae0b9d
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-20 20:44:49 +0200

    user form html views

--- page/page/settings.py
+++ page/page/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'h1.apps.H1Config',
     'about.apps.AboutConfig',
     'blog.apps.BlogConfig',
