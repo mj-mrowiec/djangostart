da7fc80a494929bf2006e74c37d119f39817536e
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-20 21:51:49 +0200

    adding submit

--- page/users/views.py
+++ page/users/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        print('Alpha')
+        print(str(form.is_valid()))
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            print('Betta')
+            messages.success(request, f'Accout Created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form':form})
+
+#messages.debug
+#messages.success
+#messages.warning
+#messages.error
