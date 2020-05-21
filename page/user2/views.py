aa0c2701f65c77f39d0782deabcf433ba724dc54
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-21 21:05:50 +0200

    expanding user registration form by adding email field

--- page/user2/views.py
+++ page/user2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 def register2(request):
-    form = UserCreationForm()
+    #form = UserCreationForm()
+    form = UserRegistrationForm()
     return render(request, 'user2/register.html', {'form': form})
