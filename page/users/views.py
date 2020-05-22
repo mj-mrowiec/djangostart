53550ab9852a26cc99d6471d60eca3af95d1430f
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-22 20:34:46 +0200

    app user3

--- page/users/views.py
+++ page/users/views.py
@@ -7,6 +7,7 @@ from .forms import UserRegisterForm
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+        
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
