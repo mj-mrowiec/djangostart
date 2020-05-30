df769fae99872075c819088a79a7190e0dc08cc1
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-30 22:04:30 +0200

    creating signals

---
+++ page/my_signal/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+def profile_save(sender, instance, created, **kwargs):
+    if created:
+        Profile.object.create(user = instance)
+
+def profile_update(sender, instance, created, **kwargs):
+    if created == False:
+        instance.profile.save()
