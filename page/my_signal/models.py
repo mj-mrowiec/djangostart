07391f050ab92f14c1dc0af1d193466cf9d06342
mj-mrowiec <mjmrowiec@gmail.com>
2020-05-30 22:34:52 +0200

    view for test user

--- page/my_signal/models.py
+++ page/my_signal/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -17,3 +15,5 @@ def profile_save(sender, instance, created, **kwargs):
 def profile_update(sender, instance, created, **kwargs):
     if created == False:
         instance.profile.save()
+
+post_save.connect(profile_save, sender = User)
